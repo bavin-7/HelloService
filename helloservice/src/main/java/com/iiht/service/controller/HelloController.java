f2d6851f056129d6da26012d0d1a6e57898020c3
Bavin <Ustjavabootan27@mml.local>
2024-09-05 11:04:27 +0530

    added

---
+++ helloservice/src/main/java/com/iiht/service/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.iiht.service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/service")
+public class HelloController {
+	
+	@GetMapping("/message")
+	public String hello() {
+		return "hello";
+	}
+	
+}
